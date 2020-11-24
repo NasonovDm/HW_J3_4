045b112d55e8d5bba74bb1850c44c05fb0b4e81b
Nasonov Dmitriy <onlydmitriy@gmail.com>
2020-11-25 00:31:13 +0300

    Initial commit

---
+++ src/main/java/HW_J3_3/Main.java
@@ -0,0 +1,13 @@
+package HW_J3_3;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    
+}
